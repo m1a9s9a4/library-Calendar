eeef3423d5ac0d6467a0cac88093a07174a1aef9
masafumi kato <xslash1105@gmail.com>
2019-05-18 22:20:17 +0900

    [Add] composer & serviceprovider

---
+++ src/laravel/ServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Library\laravel;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 動作確認用メッセージ
+        dump('Hello Laravel!');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
